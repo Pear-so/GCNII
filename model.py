ea622b01cb68b89ddd8a5a64911e3f2af8a0e7ba
Pear-so <74841713+Pear-so@users.noreply.github.com>
2024-07-27 22:06:41 +0800

    Update model.py

--- model.py
+++ model.py
@@ -70,13 +70,13 @@ class GCNIIppi(nn.Module):
     def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha,variant):#输入特征数量，图卷积层数量，每层的隐藏单元数，输出类别数量，dropout比率，超参，超参数，指示是否使用变体GCNII*
         super(GCNIIppi, self).__init__()
         self.convs = nn.ModuleList()# ModuleList是用于存储子模块的特殊列表
-        for _ in range(nlayers):#循环 nlayers 次，表示要创建的图卷积层的数量，
+        for _ in range(nlayers):#循环 nlayers 次，表示要创建的图卷积层的数量
             self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant,residual=True))
-        self.fcs = nn.ModuleList()
-        self.fcs.append(nn.Linear(nfeat, nhidden))
-        self.fcs.append(nn.Linear(nhidden, nclass))
-        self.act_fn = nn.ReLU()
-        self.sig = nn.Sigmoid()
+        self.fcs = nn.ModuleList()#创建另一个 ModuleList，用于存储全连接层
+        self.fcs.append(nn.Linear(nfeat, nhidden))#第一个全连接层，它将输入特征从维度 nfeat 转换到 nhidden。
+        self.fcs.append(nn.Linear(nhidden, nclass))#第二个全连接层，它将输入特征从维度  nhidden 转换到输出类别的数量 nclass
+        self.act_fn = nn.ReLU()#定义一个ReLU激活函数
+        self.sig = nn.Sigmoid()#定义一个Sigmoid激活函数
         self.dropout = dropout
         self.alpha = alpha
         self.lamda = lamda
@@ -84,11 +84,11 @@ class GCNIIppi(nn.Module):
     def forward(self, x, adj):
         _layers = []
         x = F.dropout(x, self.dropout, training=self.training)
-        layer_inner = self.act_fn(self.fcs[0](x))
-        _layers.append(layer_inner)
-        for i,con in enumerate(self.convs):
-            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
-            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = self.act_fn(self.fcs[0](x))#通过第一个全连接层将输入特征转换到隐藏层的维度，然后应用ReLU激活函数
+        _layers.append(layer_inner)#将结果存储在 _layers 列表中，这个列表用于在图卷积层中传递跨层信息
+        for i,con in enumerate(self.convs):#循环遍历图卷积层列表 self.convs，对每一层的输出应用Dropout和ReLU激活函数
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)#layer_inner前一层的输出特征
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))#_layers[0]：当前示例代码中，这里似乎是一个错误
         layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
         layer_inner = self.sig(self.fcs[-1](layer_inner))
         return layer_inner
